3ffc823a76932dd7a5ba6359a6fe83ce89a5f0eb
Fertzyyy <sarmientodeangelojazzffer@gmail.com>
2024-12-21 11:22:58 +0800

    Delete LA directory

--- LA/3/LA03_SARMIENTO.py
+++
@@ -1,9 +0,0 @@
-class character():
-    def __init__(self,name,):
-        self.name = name
-    
-    def describe(self, role):
-        print(f"{hero.name} is a {role} hero.")
-               
-hero = character("Layla")
-hero.describe("Marksman")
